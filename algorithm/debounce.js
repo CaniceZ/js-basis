65055271db8747255a437c8ee9cacb6b5fc298df
zhengchuangjun <349373391@qq.com>
2022-07-26 14:20:10 +0800

    fix: 防抖节流bug

--- algorithm/debounce.js
+++ algorithm/debounce.js
@@ -1,14 +1,21 @@
-// 防抖
+// 防抖 时间段内多次触发只执行最后一次
 function debounce(fn, wait) {
     var timeout = null;
     return function() {
+        let context = this, args = [...arguments];
         if(timeout !== null)  clearTimeout(timeout); 
-        timeout = setTimeout(fn, wait);
+        timeout = setTimeout(function(){
+            fn.apply(context, args);
+            timeout = null;            
+        }, wait)    
     }
 }
 // 处理函数
-function handle() {
-    console.log(Math.random()); 
+function handle(x) {
+    return function(){
+        console.log(x); 
+    }
+    
 }
 // 滚动事件
 window.addEventListener('scroll', debounce(handle, 1000));
--- algorithm/throttle.js
+++ algorithm/throttle.js
@@ -1,17 +1,42 @@
 // 节流-间隔时间内多次点击，只会按间隔时间触发
 
-function throttle(fn,times = 300){
-    let bool = true
+// function throttle(fn,times = 300){
+//     let bool = true
+//     return function(){
+//         if(!bool){
+//            return false
+//       }
+//       setTimeout(()=>{
+//            bool = true
+//            fn.apply(this,arguments)
+//       },times)
+//   }
+// }
+// 时间戳版
+// function throttle(func, wait){
+//     let previous = 0;
+//     return function(){
+//         let now = Date.now(), context = this, args = [...arguments];
+//         if(now - previous > wait){
+//             func.apply(context, args);
+//             previous = now; // 闭包，记录本次执行时间戳      
+//         }    
+//     }        
+// }
+
+// 定时器版
+function throttle(func, wait){
+    let timer = null;
     return function(){
-        if(!bool){
-           return false
-      }
-      setTimeout(()=>{
-           bool = true
-           fn.apply(this,arguments)
-      },times)
-  }
+        let context = this, args = [...arguments];
+        if(timer) return;
+        timer = setTimeout(function(){
+            func.apply(context, args);
+            timer = null;            
+        }, wait)    
+    }
 }
+
 function a(x){
     return function(){
        console.log(x)
